ef24d8eae2e3bb139bfbd9dccf691e20cdd72cda
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-11-14 12:00:51 -0300

    feat(controller): add product controller with CRUD operations
    Implement ProductController class with full CRUD functionality including:
    - Create, read, update and delete operations
    - Helper methods for ID generation and product search
    - Integration with existing menu system
    - Console logging with color formatting

--- Menu.ts
+++ Menu.ts
@@ -2,23 +2,26 @@ import readlinesync = require("readline-sync")
 import { colors } from './src/util/Colors'
 import { Headset } from './src/model/Headset'
 import { Teclado } from './src/model/Teclado'
+import { ProdutoController } from './src/controller/ProdutoController'
 
 export function main() {
 
     // Variaveis Auxiliares
     let opcao: number
 
-    // Produtos mockados para teste
+    // Instância do Controller para gerenciar os produtos
+    const controller = new ProdutoController()
+    
+    // Produtos mockados
     const morcegoPro = new Headset(1, "Morcego Pro Wireless", 499.90, "Headset Gamer", "Wireless", true)
     const panteraPro = new Teclado(2, "Pantera Pro TKL", 349.90, "Teclado Mecânico", "TKL", "Red Switch")
     const morcegoV2 = new Headset(3, "Morcego V2", 299.90, "Headset Gamer", "Com Fio", true)
     const panteraEco = new Teclado(4, "Pantera Eco Light", 199.90, "Teclado Membrana", "100%", "Membrana")
-
-    // Exibir produtos mockados
-    morcegoPro.visualizar()
-    panteraPro.visualizar()
-    morcegoV2.visualizar()
-    panteraEco.visualizar()
+    
+    controller.cadastrar(morcegoPro)
+    controller.cadastrar(panteraPro)
+    controller.cadastrar(morcegoV2)
+    controller.cadastrar(panteraEco)
 
     while (true) {
 
@@ -55,30 +58,120 @@ export function main() {
                 console.log(colors.fg.whitestrong,
                     "\n\nCadastrar Produto\n\n", colors.reset)
                 
+                console.log("Digite o tipo de produto (1-Headset, 2-Teclado):")
+                let tipoProduto = readlinesync.questionInt("")
+                
+                console.log("Digite o nome do produto:")
+                let nome = readlinesync.question("")
+                
+                console.log("Digite o preço do produto:")
+                let preco = readlinesync.questionFloat("")
+                
+                let id = controller.gerarId()
+                
+                if (tipoProduto == 1) {
+                    console.log("Digite a conectividade (Wireless/Com Fio):")
+                    let conectividade = readlinesync.question("")
+                    
+                    console.log("Possui RGB? (s/n):")
+                    let possuiRGB = readlinesync.question("").toLowerCase() === 's'
+                    
+                    let headset = new Headset(id, nome, preco, "Headset Gamer", conectividade, possuiRGB)
+                    controller.cadastrar(headset)
+                    
+                } else if (tipoProduto == 2) {
+                    console.log("Digite o formato (TKL/100%):")
+                    let formato = readlinesync.question("")
+                    
+                    console.log("Digite o tipo de switch (Red/Blue/Brown):")
+                    let tipoSwitch = readlinesync.question("")
+                    
+                    let teclado = new Teclado(id, nome, preco, "Teclado Mecânico", formato, tipoSwitch)
+                    controller.cadastrar(teclado)
+                    
+                } else {
+                    console.log(colors.fg.red, "Tipo de produto inválido!", colors.reset)
+                }
+                
                 keyPress()
                 break
             case 2:
                 console.log(colors.fg.whitestrong,
                     "\n\nListar todos os Produtos\n\n", colors.reset)
 
+                controller.listarTodos()
                 keyPress()
                 break
             case 3:
                 console.log(colors.fg.whitestrong,
                     "\n\nBuscar Produto por ID\n\n", colors.reset)
 
+                console.log("Digite o ID do produto:")
+                let idBusca = readlinesync.questionInt("")
+                controller.procurarPorId(idBusca)
+                
                 keyPress()
                 break
             case 4:
                 console.log(colors.fg.whitestrong,
                     "\n\nAtualizar dados do Produto\n\n", colors.reset)
 
+                console.log("Digite o ID do produto que deseja atualizar:")
+                let idAtualizar = readlinesync.questionInt("")
+                
+                let produtoExistente = controller.buscarNoArray(idAtualizar)
+                
+                if (produtoExistente != null) {
+                    console.log("Produto encontrado:")
+                    produtoExistente.visualizar()
+                    
+                    console.log("\nDigite o novo nome do produto:")
+                    let novoNome = readlinesync.question("")
+                    
+                    console.log("Digite o novo preço do produto:")
+                    let novoPreco = readlinesync.questionFloat("")
+                    
+                    produtoExistente.nome = novoNome
+                    produtoExistente.preco = novoPreco
+                    
+                    if (produtoExistente instanceof Headset) {
+                        console.log("Digite a nova conectividade (Wireless/Com Fio):")
+                        let novaConectividade = readlinesync.question("")
+                        
+                        console.log("Possui RGB? (s/n):")
+                        let novoRGB = readlinesync.question("").toLowerCase() === 's'
+                        
+                        produtoExistente.conectividade = novaConectividade
+                        produtoExistente.possuiRGB = novoRGB
+                        
+                    } else if (produtoExistente instanceof Teclado) {
+                        console.log("Digite o novo formato (TKL/100%):")
+                        let novoFormato = readlinesync.question("")
+                        
+                        console.log("Digite o novo tipo de switch (Red/Blue/Brown):")
+                        let novoSwitch = readlinesync.question("")
+                        
+                        produtoExistente.formato = novoFormato
+                        produtoExistente.tipoSwitch = novoSwitch
+                    }
+                    
+                    controller.atualizar(produtoExistente)
+                    
+                } else {
+                    console.log(colors.fg.red, "Produto não encontrado!", colors.reset)
+                }
+                
                 keyPress()
                 break
             case 5:
                 console.log(colors.fg.whitestrong,
                     "\n\nApagar Produto\n\n", colors.reset)
 
+                console.log("Digite o ID do produto que deseja apagar:")
+                let idApagar = readlinesync.questionInt("")
+                
+                controller.deletar(idApagar)
+                
                 keyPress()
                 break
             default:
---
+++ src/controller/ProdutoController.ts
@@ -0,0 +1,73 @@
+import { Produto } from "../model/Produto"
+import { ProdutoRepository } from "../repository/ProdutoRepository"
+import { colors } from "../util/Colors"
+
+export class ProdutoController implements ProdutoRepository {
+
+    private listaProdutos: Array<Produto> = new Array<Produto>()
+    numero: number = 0
+
+    // Métodos do CRUD
+    procurarPorId(id: number): void {
+        let buscaProduto = this.buscarNoArray(id)
+        
+        if (buscaProduto != null) {
+            buscaProduto.visualizar()
+        } else {
+            console.log(colors.fg.red, "\nProduto com ID " + id + " não foi encontrado!", colors.reset)
+        }
+    }
+
+    listarTodos(): void {
+        if (this.listaProdutos.length === 0) {
+            console.log(colors.fg.yellow, "\nNão há produtos cadastrados!", colors.reset)
+            return
+        }
+        
+        for (let produto of this.listaProdutos) {
+            produto.visualizar()
+        }
+    }
+
+    cadastrar(produto: Produto): void {
+        this.listaProdutos.push(produto)
+        console.log(colors.fg.green, "\nProduto \"" + produto.nome + "\" foi cadastrado com sucesso!", colors.reset)
+    }
+
+    atualizar(produto: Produto): void {
+        let buscaProduto = this.buscarNoArray(produto.id)
+
+        if (buscaProduto != null) {
+            this.listaProdutos[this.listaProdutos.indexOf(buscaProduto)] = produto
+            console.log(colors.fg.green, "\nProduto \"" + produto.nome + "\" foi atualizado com sucesso!", colors.reset)
+        } else {
+            console.log(colors.fg.red, "\nProduto com ID " + produto.id + " não foi encontrado!", colors.reset)
+        }
+    }
+
+    deletar(id: number): void {
+        let buscaProduto = this.buscarNoArray(id)
+        
+        if (buscaProduto != null) {
+            this.listaProdutos.splice(this.listaProdutos.indexOf(buscaProduto), 1)
+            console.log(colors.fg.green, "\nProduto com ID " + id + " foi deletado com sucesso!", colors.reset)
+        } else {
+            console.log(colors.fg.red, "\nProduto com ID " + id + " não foi encontrado!", colors.reset)
+        }
+    }
+
+    // Métodos auxiliares
+    public buscarNoArray(id: number): Produto | null {
+        for (let produto of this.listaProdutos) {
+            if (produto.id === id) {
+                return produto
+            }
+        }
+        return null
+    }
+
+    public gerarId(): number {
+        return ++this.numero
+    }
+
+}
